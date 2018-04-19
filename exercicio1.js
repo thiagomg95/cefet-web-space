b8adb9673ef194494d94a27ac55c7e5d01827f55
thiagomg95 <37165261+thiagomg95@users.noreply.github.com>
2018-04-19 11:51:28 -0300

    Update exercicio1.js

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,21 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+let changeExpansao = document.getElementsByClassName('botao-expandir-retrair');
+
+for(let botao of changeExpansao)
+{
+  botao.addEventListener('click', function() {
+    let paragrafo = botao.parentNode;
+    //Se nao esta expandido
+    if (paragrafo.classList.contains('expandido'))
+    {
+      paragrafo.classList.remove('expandido');
+      botao.innerHTML = "+";
+    }
+    else
+    {
+      paragrafo.classList.add('expandido');
+      botao.innerHTML = "-";
+    }
+  });
+}
