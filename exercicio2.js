b02fd364400fdf1ac47ab83e8a4b10b706e06253
thiagomg95 <37165261+thiagomg95@users.noreply.github.com>
2018-06-12 17:49:39 -0300

    Update exercicio2.js

--- exercicio2.js
+++ exercicio2.js
@@ -10,18 +10,6 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-collecting.jpg'
   ];
 
-// Faça o exercício da galeria de imagens aqui
-// Este arquivo ainda não está incluído no arquivo HTML
-
-const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
-  todasAsImagens = [
-    'philae-parts.jpg',
-    'philae-rosetta.jpg',
-    'philae-separation.jpg',
-    'philae-67-picture.jpg',
-    'philae-collecting.jpg'
-  ];
-
 let valor = 0;
 
 function navega(direcao)
